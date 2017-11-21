95442c03f82228fc5b8a8de188538eabadb044b6
Nuri Özdaş <32783197+globalaplication@users.noreply.github.com>
2017-11-21 13:39:49 +0300

    Update fsearch.py

--- fsearch.py
+++ fsearch.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python3
+
 import os
+
 def folder_test(path):
 	try:
 		if len(os.listdir(path)) >= 0: return 1
 	except:
 		return -1
+
 def text_search(file, text):
 	try:
 		file = open(file)
@@ -15,22 +18,30 @@ def text_search(file, text):
 			return 1
 	except:
 		pass
+
+def ffind(string, fstr):
+	if string.find(fstr) is not -1:
+		return 1 
+	else: 
+		return -1
+
 def fsearch(path, search_string, type, dosdevices=False, PlayOnLinux=False, wine=False):
 	print('Lütfen bekleyiniz...')
-
-	if dosdevices is False: dosdevices = -1
-	if PlayOnLinux is False: PlayOnLinux = -1
-	if wine is False: wine = -1
-
+	if dosdevices is False: 
+		dosdevices = -1 
+	if PlayOnLinux is False: 
+		PlayOnLinux = -1
+	if wine is False: 
+		wine = -1
 	dir_list = [path]
 	for path in dir_list:
 		list = os.listdir(path)
 		select = [
-			path+'/'+select for select in list 
-			if folder_test(path+'/'+select) is 1 
-			if path.find('/dosdevices/') is dosdevices if path.find('PlayOnLinux') is PlayOnLinux
-			if path.find('/.wine') is wine
-		]
+				path+'/'+select for select in list 
+				if folder_test(path+'/'+select) is 1 if ffind(path, '/dosdevices/') is dosdevices 
+				if ffind(path, 'PlayOnLinux') is PlayOnLinux 
+				if ffind(path, '/.wine') is wine
+														]
 		dir_list.extend(select)
 		for select in select:
 			for beta in os.listdir(select):
@@ -38,4 +49,6 @@ def fsearch(path, search_string, type, dosdevices=False, PlayOnLinux=False, wine
 					if str(select+'/'+beta).find(fextention) is not -1 and text_search(select+'/'+beta, search_string) is 1:
 						print (select+'/'+beta)
 
-fsearch('/home/linuxmt', 'aranacak kelime', '.txt .py', False, False, False)
+
+						
+fsearch('/home', 'topla', '.txt .py', False, False, False)
