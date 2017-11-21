a17bcf610f775332f20b60a58c77222ad881faf4
Nuri Özdaş <32783197+globalaplication@users.noreply.github.com>
2017-11-21 04:39:38 +0200

    Add files via upload

---
+++ fsearch.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+import os
+def folder_test(path):
+	try:
+		if len(os.listdir(path)) >= 0: return 1
+	except:
+		return -1
+def text_search(file, text):
+	try:
+		file = open(file)
+		source = file.read()
+		file.close()
+		if source.find(text) is not -1:
+			return 1
+	except:
+		pass
+def fsearch(path, search_string, type, dosdevices=False, PlayOnLinux=False, wine=False):
+	print('Lütfen bekleyiniz...')
+	if dosdevices is False: dosdevices = -1
+	if PlayOnLinux is False: PlayOnLinux = -1
+	if wine is False: wine = -1
+	dir_list = [path]
+	for path in dir_list:
+		list = os.listdir(path)
+		select = [path+'/'+select for select in list 
+					if folder_test(path+'/'+select) is 1 
+				if path.find('/dosdevices/') is dosdevices if path.find('PlayOnLinux') is PlayOnLinux
+					if path.find('/.wine') is wine]
+		dir_list.extend(select)
+		for select in select:
+			for beta in os.listdir(select):
+				for fextention in type.split():
+					if str(select+'/'+beta).find(fextention) is not -1 and text_search(select+'/'+beta, search_string) is 1:
+						print (select+'/'+beta)
+
+fsearch('/home/linuxmt', 'forum', '.txt .py .dat', False, False, False)
